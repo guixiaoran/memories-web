a48b610c45a1561aa6f8e6869ec7ed614a637b5a
Sanchit Dang <sanchitd5@live.com>
2020-09-14 14:14:54 +1000

    scroll bug fix

--- src/components/dependants/Header.js
+++ src/components/dependants/Header.js
@@ -11,24 +11,24 @@ export const HeaderTop = () => {
     <div className="burger" onClick={(e) => {
       if (!e.target.classList.contains("active")) {
         e.target.classList.add("active")
-        gsap.to(".line1", 0.3, { rotate: "45", y: 5, background: "black" });
-        gsap.to(".line2", 0.3, { rotate: "-45", y: -5, background: "black" });
+        gsap.to("#line1", 0.3, { rotate: "45", y: 5, background: "black" });
+        gsap.to("#line2", 0.3, { rotate: "-45", y: -5, background: "black" });
         gsap.to(".nav-bar", .75, { clipPath: "circle(4500px at 100% -10%)" })
         gsap.to("#logo", 1, { color: "black" })
         document.body.classList.add("hide")
         document.getElementById("logo").style.display = "none"
       } else {
         e.target.classList.remove("active")
-        gsap.to(".line1", 0.3, { rotate: "0", y: 0, background: "white" });
-        gsap.to(".line2", 0.3, { rotate: "0", y: 0, background: "white" });
+        gsap.to("#line1", 0.3, { rotate: "0", y: 0, background: "white" });
+        gsap.to("#line2", 0.3, { rotate: "0", y: 0, background: "white" });
         gsap.to(".nav-bar", .75, { clipPath: "circle(50px at 100% -10%)" })
         gsap.to("#logo", 1, { color: "white" })
         document.getElementById("logo").style.display = "block"
         document.body.classList.remove("hide")
       }
     }}>
-      <div className="line1"></div>
-      <div className="line2"></div>
+      <div id="line1" className="line1"></div>
+      <div id="line2" className="line2"></div>
     </div>
   </header>;
 }
@@ -54,7 +54,14 @@ export const HeaderBottom = () => {
       <ul className="nav-links">
         {
           menuItems.map((value, i) => {
-            return <Link key={`link${i}`} to={value.controller} className="link-views video-stories-link">
+            return <Link onClick={() => {
+              gsap.to("#line1", 0.3, { rotate: "0", y: 0, background: "white" });
+              gsap.to("#line2", 0.3, { rotate: "0", y: 0, background: "white" });
+              gsap.to(".nav-bar", .75, { clipPath: "circle(50px at 100% -10%)" })
+              gsap.to("#logo", 1, { color: "white" })
+              document.getElementById("logo").style.display = "block"
+              document.body.classList.remove("hide")
+            }} key={`link${i}`} to={value.controller} className="link-views video-stories-link">
               <h3>{value.name}</h3>
             </Link>;
           })
