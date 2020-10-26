0c38d8b7917e565ed223af07b1fb1f3c70a906d6
Sanchit Dang <sanchitd5@live.com>
2020-10-26 16:38:35 +1100

    add images to button

--- src/views/dependants/Navigator/Navigator.js
+++ src/views/dependants/Navigator/Navigator.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Link } from 'react-router-dom';
-import { Button, makeStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 import { AnimatedObject } from "helpers/index";
 import { HeaderContext } from "contexts";
 import * as navigatorBG from "assets/img/navigatorBG.png";
+import * as archiveBG from "assets/img/archives.jpg";
+import * as memoryWalksBG from "assets/img/memoryWalks.png";
+import * as plazaBG from "assets/img/plaza.png";
+import * as videoStoriesBG from "assets/img/videoStories.png";
+import * as documentaryBG from "assets/img/documentary.png";
 
 const useStyles = makeStyles(() => ({
+  first: {
+    borderRadius: '10px 0 0 0',
+  },
+  last: {
+    borderRadius: '0 0 0 10px',
+  },
   sidenav: {
     zIndex: 99,
     '& .MuiButton-label': {
@@ -13,54 +24,94 @@ const useStyles = makeStyles(() => ({
     },
     '& a': {
       position: "absolute",
-      left: window.innerWidth - 150,
+      left: window.innerWidth - 200,
       transition: "0.3s",
       padding: "15px",
-      width: "200px",
+      height: "100px",
+      width: "220px",
       textDecoration: "none",
       color: 'black',
-      borderRadius: '5px 0 0 5px',
+      opacity: 0.5,
+      backgroundSize: 'auto',
+      backgroundPosition: 'center'
     },
     '& a:hover': {
-      left: window.innerWidth - 200
+      left: window.innerWidth - 220,
+      opacity: 1
+
+    },
+    label: {
+
     }
   }
 }));
 
 const ButtonWithLink = (props) => {
   const { setDisplayBackButton } = useContext(HeaderContext);
+  const classes = useStyles();
 
   let button = (
-    <Button lableStyle={{ float: 'left', justifyContent: "left" }} onClick={() => {
+    <Link to={props.to} className={props.index === 0 ? classes.first : (props.index === props.size - 1 ? classes.last : '')
+    } onClick={() => {
       setDisplayBackButton(true);
     }} style={{
-      top: ((window.innerHeight / 2) - (window.innerHeight / 6)) + props.index * 60,
-    }} fullWidth variant="contained" component={Link} to={props.to}>
-      {props.children}
-    </Button>
+      backgroundImage: `url(${props.image})`,
+      top: ((window.innerHeight / 2) - (window.innerHeight / 3)) + props.index * 100,
+    }} >
+      <span style={{
+        float: 'right',
+        justifyContent: "right",
+        margin: "0 auto",
+        paddingRight: '10px',
+        color: 'white'
+      }}>
+        {props.children}
+      </span>
+    </Link >
   );
   return button;
 };
 
 const SideNav = () => {
   const classes = useStyles();
+  const [buttons] = useState([
+    {
+      title: "Documentary",
+      image: documentaryBG,
+      to: "documentary"
+    },
+    {
+      title: "Video Stories",
+      image: videoStoriesBG,
+      to: "videostories"
+    },
+    {
+      title: "Memory Walks",
+      image: memoryWalksBG,
+      to: "memorywalks"
+    },
+    {
+      title: "Archive",
+      image: archiveBG,
+      to: "archive"
+    },
+    {
+      title: "Plaza",
+      image: plazaBG,
+      to: "plaza"
+    }
+  ]);
   return (
     <div id="navigationSidebar" className={classes.sidenav}>
-      <ButtonWithLink index={0} to="documentary">
-        Documentary
-      </ButtonWithLink>
-      <ButtonWithLink index={1} to="videostories">
-        Video Stories
-      </ButtonWithLink>
-      <ButtonWithLink index={2} to="memorywalks">
-        Memory Walks
-      </ButtonWithLink>
-      <ButtonWithLink index={3} to="archive">
-        archive
-      </ButtonWithLink>
-      <ButtonWithLink index={4} to="plaza">
-        Plaza
-      </ButtonWithLink>
+      {
+        buttons.map((button, i) => <ButtonWithLink
+          key={`button_${i}`}
+          image={button.image} index={i}
+          to={button.to}
+          size={buttons.length}>
+          {button.title}
+        </ButtonWithLink>)
+      }
     </div>
   );
 };
@@ -87,7 +138,7 @@ export const Navigator = () => {
           <div className="parallax hero" style={{
             overflow: "hidden"
           }}>
-            <video playsInline loop id="homeVideo" muted autoPlay style={{
+            {/* <video playsInline loop id="homeVideo" muted autoPlay style={{
               height: 'auto',
               minHeight: window.innerHeight + 110,
               maxWidth: window.innerWidth,
@@ -95,7 +146,7 @@ export const Navigator = () => {
             }}>
               <source src={videoLink} type="video/mp4" />
           Your browser does not support HTML5 video.
-            </video>
+            </video> */}
             <SideNav />
           </div>
         </section>
