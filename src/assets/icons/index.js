dfc93e50c448940bb7620955d49831f2349fcee7
Sanchit Dang <sanchitd5@live.com>
2020-10-27 10:39:50 +1100

    add archive icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,23 @@
+import * as BroadcastLine from "./broadcast-line.png";
+import * as BroadcastLine2x from "./broadcast-line@2x.png";
+import * as FilmLine from "./film-line.png";
+import * as FilmLine2x from "./film-line@2x.png";
+import * as MemoryWalks from "./memorywalks.png";
+import * as MemoryWalks2x from "./memorywalks@2x.png";
+import * as VideoStories from "./videostories.png";
+import * as VideoStories2x from "./videostories@2x.png";
+import * as Vidicon from "./vidicon-2-line.png";
+import * as Vidicon2x from "./vidicon-2-line@2x.png";
+
+export {
+  BroadcastLine,
+  BroadcastLine2x,
+  FilmLine,
+  FilmLine2x,
+  MemoryWalks,
+  MemoryWalks2x,
+  VideoStories,
+  VideoStories2x,
+  Vidicon,
+  Vidicon2x
+};
---
+++ src/assets/index.js
@@ -0,0 +1,25 @@
+import {
+  BroadcastLine,
+  BroadcastLine2x,
+  FilmLine,
+  FilmLine2x,
+  MemoryWalks,
+  MemoryWalks2x,
+  VideoStories,
+  VideoStories2x,
+  Vidicon,
+  Vidicon2x
+} from "./icons";
+
+export {
+  BroadcastLine,
+  BroadcastLine2x,
+  FilmLine,
+  FilmLine2x,
+  MemoryWalks,
+  MemoryWalks2x,
+  VideoStories,
+  VideoStories2x,
+  Vidicon,
+  Vidicon2x
+}
