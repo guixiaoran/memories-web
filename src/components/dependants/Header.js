456359d019be1748f1f6baf64ede647ae9b0d9e0
Sanchit Dang <sanchitd5@live.com>
2020-10-26 16:43:23 +1100

    set cursor of backbutton to pointer

--- src/components/dependants/Header.js
+++ src/components/dependants/Header.js
@@ -8,7 +8,9 @@ import { Animator } from 'helpers/index';
 export const HeaderTop = withRouter((props) => {
   const { displayBackButton, setDisplayBackButton } = useContext(HeaderContext);
   return <header className="nav-header" style={{ paddingLeft: "5vw", paddingRight: "5vw" }}>
-    <h1>{displayBackButton ? <span onClick={() => {
+    <h1>{displayBackButton ? <span style={{
+      cursor: 'pointer'
+    }} onClick={() => {
       props.history.goBack();
     }} id="logo">{`<- Back`}</span> : <Link style={{
       cursor: "pointer"
