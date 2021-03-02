ee333881fd432fd4fcf8aec0c1c5fada303ee0d7
Sanchit Dang <sanchitd5@live.com>
2021-03-02 12:45:12 +1100

    Redesign Project Story Page

--- src/views/dependants/ProjectStory/Stories.js
+++ src/views/dependants/ProjectStory/Stories.js
@@ -1,25 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import { LoadingScreen } from 'components/index';
-import { Container, Grid, Typography, useMediaQuery, Card, CardHeader, CardMedia } from '@material-ui/core';
+import { LoadingScreen, Image } from 'components/index';
+import { Container, Grid, CardContent, Typography, useMediaQuery, Card, CardHeader } from '@material-ui/core';
 import { API, TextHelper } from 'helpers';
-import { Link } from 'react-router-dom';
-
 
 const StoryCard = (props) => {
-
   let content = (
-    <Link to={`/story/${props.story._id}`}>
-      <Card >
+    <Card style={{ height: '100%', borderRadius: '0' }} >
+      <CardHeader title={TextHelper.titleCase(props.story.title)} />
+      <CardContent style={{ margin: '0 auto' }}>
+
+        <Image src={props.story.thumb} />
+      </CardContent>
+    </Card>
+  );
+  return content;
+};
+
+const Story = (props) => {
+  let content = (
+    <Card style={{
+      height: '100%', borderRadius: '0',
+      background: "rgba(255,255,255,0.5)"
+    }}>
+      <CardContent>
         <CardHeader title={TextHelper.titleCase(props.story.title)} />
-        <CardMedia component="img" src={props.story.thumb} />
-      </Card>
-    </Link>
+        <div style={{ padding: '10px' }} dangerouslySetInnerHTML={{ __html: props.story.story }} />
+      </CardContent>
+    </Card>
   );
   return content;
 };
 
 export const Stories = () => {
   const [stories, setStories] = useState();
+  const [videoLink] = useState("https://s3.au-syd.cloud-object-storage.appdomain.cloud/memories/video/video/original/Video_OfSmDJNpZSnZ.mp4");
+
   let isItDesktop = useMediaQuery('(min-width:600px) and (min-height:600px)');
   useEffect(() => {
     API.getProjectStory((response) => {
@@ -31,20 +46,54 @@ export const Stories = () => {
   if (stories === undefined) return <LoadingScreen />;
 
   let content = (
-    <Container style={{ marginTop: isItDesktop ? '7%' : '20%', marginBottom: isItDesktop ? '10%' : '20%' }}>
-      <Grid container spacing={1}>
-        <Grid item xs={12} style={{ marginBottom: '20px' }}>
-          <Typography variant='h4' style={{ color: 'white' }}>
-            The stories of the project
-          </Typography>
+    <div>
+      <video loop playsInline id="homeVideo" muted autoPlay style={{
+        height: 'auto',
+        minHeight: '100vh',
+        maxWidth: '100vw',
+        objectFit: "cover",
+        position: 'fixed',
+        top: 0,
+        left: 0
+      }}>
+        <source src={videoLink} type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video>
+      <Container style={{
+        position: 'relative',
+        marginTop: isItDesktop ? '7%' : '20%', marginBottom: isItDesktop ? '10%' : '20%'
+      }}>
+        <Grid container spacing={0}>
+          <Grid item xs={12} style={{ marginBottom: '20px' }}>
+            <Typography variant='h4' style={{ color: 'white' }}>
+              The stories of the project
+            </Typography>
+          </Grid>
+          {
+            stories.map((story, i) => {
+              if (i % 2 === 0) {
+                return <>
+                  <Grid item xs={12} sm={6} key={`story_${i}`}>
+                    <Story story={story} />
+                  </Grid>
+                  <Grid item xs={12} sm={6} key={`story_${i}`}>
+                    <StoryCard story={story} />
+                  </Grid>
+                </>;
+              }
+              return <>
+                <Grid item xs={12} sm={6} key={`story_${i}`}>
+                  <StoryCard story={story} />
+                </Grid>
+                <Grid item xs={12} sm={6} key={`story_${i}`}>
+                  <Story story={story} />
+                </Grid>
+              </>;
+            })
+          }
         </Grid>
-        {
-          stories.map((story, i) => <Grid item xs={12} sm={4} key={`story_${i}`}>
-            <StoryCard story={story} />
-          </Grid>)
-        }
-      </Grid>
-    </Container>
+      </Container>
+    </div >
   );
 
   return content;
