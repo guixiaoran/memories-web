378ef08de9cef927da2034c23333abb81af976e4
alessio <alexbonti83@hotmail.com>
2021-03-17 21:31:11 +1100

    added music and changed a few buildings

--- src/assets/audio/index.js
+++ src/assets/audio/index.js
@@ -2,10 +2,12 @@ import * as  ConcertSound from './concert.mp3';
 import * as  ShipyardSound from './shipyard.mp3';
 import * as CircusSound from './circus.wav';
 import * as MarketSound from './market.ogg';
+import * as AllegroSound from './allegro.mp3';
 
 export {
   ConcertSound,
   ShipyardSound,
   CircusSound,
-  MarketSound
+  MarketSound,
+  AllegroSound
 };
--- src/views/dependants/Plaza/Plaza.js
+++ src/views/dependants/Plaza/Plaza.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { TextHelper, API } from "helpers";
 import { Canvas } from "react-three-fiber";
-import { Sky, PointerLockControls, softShadows, PositionalAudio } from "@react-three/drei";
+import { Sky, PointerLockControls, softShadows, PositionalAudio ,Sphere} from "@react-three/drei";
 // import { Stars } from "@react-three/drei";
 import { Physics } from "use-cannon";
 // import Tent from './tent';
@@ -11,7 +11,8 @@ import { LoadingScreen } from 'components/index';
 import moment from "moment";
 import { PlazaConfig } from "configurations";
 import { Player, Ground, Cube } from 'components/dependants/Piazza';
-import { ConcertSound } from 'assets/audio/index';
+import { AllegroSound } from 'assets/audio/index';
+
 // import { MarketSound } from 'assets/audio/index';
 import { Suspense } from 'react';
 import { Loader } from '@react-three/drei/index';
@@ -29,7 +30,7 @@ const PlazaInner = () => {
   const [cubes, setCubes] = useState();
   const fountain = useRef();
   // const marketSound = useRef();
-  const crowdSound = useRef();
+  const allegroSound = useRef();
 
   useEffect(() => {
     (() => {
@@ -38,18 +39,42 @@ const PlazaInner = () => {
         await response.sort((a, b) => moment(a.date).diff(b.date)).forEach((item, i) => {
           if (item.media.length > 0) {
             if (item.media[0].type === "image") {
-              if (item.displayIn === 'PLAZA' || item.displayIn === 'BOTH')
+              if (item.displayIn === 'PLAZA' || item.displayIn === 'BOTH'){
+                const getZoneOneZ = () => {
+                  const arr = [-28, -27, -26, -25, -24];
+                  const rand = TextHelper.getRandomInteger(-20, 20);
+                  if (!arr.includes(rand))
+                    return rand;
+                  else return getZoneOneZ();
+                };
+                const getZoneOneX = () => {
+                  const arr = [-13, -14, -15, -16, -17, -18];
+                  const rand = TextHelper.getRandomInteger(-20, 20);
+                  if (!arr.includes(rand))
+                    return rand;
+                  else return getZoneOneX();
+                };
+                tempCubes.push({
+                  position: [
+                    getZoneOneX(),
+                    0.4,
+                    getZoneOneZ(),
+                  ],
+                  media: PlazaConfig.useProxy ? `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_CORS_PORT}/${item.media[0].link}` : item.media[0].link
+                })
+              }
+              
                 if (i % 2 === 0) {
-                  const getZoneOneZ = () => {
+                 /* const getZoneOneZ = () => {
                     const arr = [-28, -27, -26, -25, -24];
-                    const rand = TextHelper.getRandomInteger(-30, -20);
+                    const rand = TextHelper.getRandomInteger(-40, -20);
                     if (!arr.includes(rand))
                       return rand;
                     else return getZoneOneZ();
                   };
                   const getZoneOneX = () => {
                     const arr = [-13, -14, -15, -16, -17, -18];
-                    const rand = TextHelper.getRandomInteger(-10, -20);
+                    const rand = TextHelper.getRandomInteger(-10, -30);
                     if (!arr.includes(rand))
                       return rand;
                     else return getZoneOneX();
@@ -61,8 +86,8 @@ const PlazaInner = () => {
                       getZoneOneZ(),
                     ],
                     media: PlazaConfig.useProxy ? `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_CORS_PORT}/${item.media[0].link}` : item.media[0].link
-                  });
-                } else {
+                  });*/
+                } else {/*
                   const getZoneTwoZ = () => {
                     const rand = TextHelper.getRandomInteger(-20, -30);
                     if (rand !== -28 && rand !== -27 && rand !== -26)
@@ -84,7 +109,7 @@ const PlazaInner = () => {
                     ],
                     media: PlazaConfig.useProxy ? `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_CORS_PORT}/${item.media[0].link}` : item.media[0].link
                   });
-                }
+                */}
 
             }
           }
@@ -102,7 +127,11 @@ const PlazaInner = () => {
 
   return (
     <Canvas shadowMap gl={{ alpha: false }} camera={{ fov: 35 }}>
-      <Sky sunPosition={[0, 5, 50]} />
+      <Sky sunPosition={[0, 5, 50]} >
+
+      </Sky>
+  
+
 
       <ambientLight intensity={0.7} />
       <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
@@ -110,43 +139,43 @@ const PlazaInner = () => {
         <Ground />
         <Player />
         <Suspense>
-          <mesh scale={[0.15, 0.15, 0.15]} rotation={[-3.08, 1.30, 3.08]} position={[28, 0, -15]}>
+          <mesh scale={[0.15, 0.15, 0.15]} rotation={[-3.08, 1.30, 3.08]} position={[23, 0, -10]}>
             <ModelX />
           </mesh >
         </Suspense>
         <Suspense>
-          <mesh scale={[0.15, 0.15, 0.15]} position={[-40, 0, -20]}>
+          <mesh scale={[0.15, 0.15, 0.15]} position={[-35, 0, -15]}>
             <ModelY />
           </mesh >
         </Suspense>
 
         <Suspense>
-          <mesh scale={[0.15, 0.15, 0.15]} rotation={[0, 0.50, 0]} position={[0, 0, -120]}>
+          <mesh scale={[0.15, 0.15, 0.15]} rotation={[0, 0.50, 0]} position={[0, 0, -115]}>
             <Model />
           </mesh >
         </Suspense>
 
         <Suspense>
-          <mesh scale={[0.07, 0.07, 0.07]} rotation={[0, 0.75, 0]} position={[-33, 0, 20]}>
+          <mesh scale={[0.07, 0.07, 0.07]} rotation={[0, 0.75, 0]} position={[-28, 0, 15]}>
             <Model2 />   
           </mesh >
         </Suspense>
 
         <Suspense>
-          <mesh scale={[0.15, 0.15, 0.15]} rotation={[0, -0.75, 0]} position={[-60, 0, -100]}>
+          <mesh scale={[0.15, 0.15, 0.15]} rotation={[0, -0.75, 0]} position={[-55, 0, -95]}>
             <Model3 />
           </mesh >
         </Suspense>
 
         <Suspense>
-          <mesh scale={[0.15, 0.15, 0.15]} rotation={[0, 0.75, 0]} position={[20, 0, 80]}>
+          <mesh scale={[0.15, 0.15, 0.15]} rotation={[0, 0.75, 0]} position={[15, 0, 75]}>
             <Model4 />
           </mesh >
         </Suspense>
 
 
         <Suspense>
-          <mesh scale={[0.15, 0.15, 0.15]} position={[0, 0, 50]}>
+          <mesh scale={[0.15, 0.15, 0.15]} position={[0, 0, 45]}>
             <Model5 />
           </mesh >
         </Suspense>
@@ -162,7 +191,7 @@ const PlazaInner = () => {
             {
               cubes.map((cube, i) => {
                 if (i / 2 === 0)
-                  return <Cube key={`${i}_cube`} imageUrl={cube.media} position={cube.position} />;
+                  return <Cube key={`${i}_cube`} imageUrl={cube.media} position={cube.position} scale={[20,20,20]}/>;
                 else return null;
               })
             }
@@ -171,18 +200,26 @@ const PlazaInner = () => {
             {
               cubes.map((cube, i) => {
                 if (i / 2 !== 0)
-                  return <Cube key={`${i}_cube`} imageUrl={cube.media} position={cube.position} />;
+                  return <Cube key={`${i}_cube`} imageUrl={cube.media} position={cube.position} scale={[20,20,20]}/>;
                 else return null;
               })
             }
           </mesh>
+          
         </Suspense>
         <Suspense>
           <mesh ref={fountain} scale={[0.01, 0.01, 0.01]} position={[2, 0, -5]}>
             <Fountain />
-            <PositionalAudio ref={crowdSound} loop distance={1} url={ConcertSound} />
           </mesh>
         </Suspense>
+        <Suspense>
+          <mesh ref={fountain} scale={[0.01, 0.01, 0.01]} position={[0, -5, -5]}>
+          <PositionalAudio ref={allegroSound} loop distance={50} url={AllegroSound} />
+
+            <Fountain />
+          </mesh>
+        </Suspense>
+       
       </Physics>
       <PointerLockControls />
     </Canvas >
